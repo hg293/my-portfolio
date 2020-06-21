fbb569d64b54798334af4faf46d26b1019562f3b
harshgs <48974981+harshgs@users.noreply.github.com>
2020-06-21 13:44:23 -0400

    Updates

---
+++ precache-manifest.6405853f2a90bafa3bef46981ca497ea.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ae964f4bc2f71b9aa3107da9e67e1e6",
+    "url": "/my-portfolio/index.html/index.html"
+  },
+  {
+    "revision": "90b3d5414847cf58015d",
+    "url": "/my-portfolio/index.html/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "258fe9ec93e54f19b938",
+    "url": "/my-portfolio/index.html/static/css/main.1da586af.chunk.css"
+  },
+  {
+    "revision": "90b3d5414847cf58015d",
+    "url": "/my-portfolio/index.html/static/js/2.03acb8be.chunk.js"
+  },
+  {
+    "revision": "3ae53551bb61526a017fad7619b64b1b",
+    "url": "/my-portfolio/index.html/static/js/2.03acb8be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "258fe9ec93e54f19b938",
+    "url": "/my-portfolio/index.html/static/js/main.1db0fded.chunk.js"
+  },
+  {
+    "revision": "f03caef257fc77d725ca",
+    "url": "/my-portfolio/index.html/static/js/runtime-main.063a71cd.js"
+  },
+  {
+    "revision": "30a9e6034a0202d6400ce2a799eb2478",
+    "url": "/my-portfolio/index.html/static/media/bmc.30a9e603.jpeg"
+  },
+  {
+    "revision": "13a5c57ed650238ff75486a699dc43fa",
+    "url": "/my-portfolio/index.html/static/media/c_programming.13a5c57e.jpg"
+  },
+  {
+    "revision": "f278be7973516129af9e64d45a0c6f4d",
+    "url": "/my-portfolio/index.html/static/media/cplus.f278be79.png"
+  },
+  {
+    "revision": "a06f60e49cb91fea42f1f3bb3d07bf27",
+    "url": "/my-portfolio/index.html/static/media/harshitha-g-dp.a06f60e4.JPG"
+  },
+  {
+    "revision": "c309c4c6a7bbdb43cf1f290786ce47ab",
+    "url": "/my-portfolio/index.html/static/media/keras-logo.c309c4c6.png"
+  },
+  {
+    "revision": "f7660c4bab802b1958fe8a8971a88d0c",
+    "url": "/my-portfolio/index.html/static/media/lab_logo.f7660c4b.png"
+  },
+  {
+    "revision": "fa79eecd83ddeed54f73bc2565ce8f8e",
+    "url": "/my-portfolio/index.html/static/media/linkedin.fa79eecd.jfif"
+  },
+  {
+    "revision": "b9639c78c83f55e00df748cb2b8abd90",
+    "url": "/my-portfolio/index.html/static/media/pandas_python.b9639c78.png"
+  },
+  {
+    "revision": "b559ee439a47b70253ceadd863e9ef8e",
+    "url": "/my-portfolio/index.html/static/media/solidworks-logo.b559ee43.png"
+  }
+]);
